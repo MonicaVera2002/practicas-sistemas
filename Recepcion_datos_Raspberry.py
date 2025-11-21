0fcb598e809e361d9864714c1eec10d20ec7b316
Mónica Vera <21150394@aguascalientes.tecnm.mx>
2025-11-21 09:47:33 -0600

    Modificación del código de recepción de datos
    Se modifico el código que permite SOLO recibir datos en la Raspberry

--- Recepcion_datos_Raspberry.py
+++ Recepcion_datos_Raspberry.py
@@ -1,10 +1,13 @@
 import serial
 
-XOR_KEY = 0x5A  # igual que Arduino
+# Se ajusta el puerto serial utilizado para esta práctica
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-ser = serial.Serial("/dev/ttyUSB0", 9600)
+#Se manda un mensaje que la Raspberry esta recibiendo los datos
+print("Raspberry lista recibiendo datos cifrados...\n")
 
+#Se comprueba que se esta recibiendo los datos y se manda solo el mensaje
 while True:
-    encrypted = ser.readline().rstrip(b"\n")
-    decrypted = bytes([b ^ XOR_KEY for b in encrypted])
-    print("Decrypted:", decrypted.decode())
+    if ser.in_waiting > 0:
+        data = ser.readline().decode().strip()
+        print("Cifrado recibido:", data)
